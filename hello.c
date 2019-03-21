86def91007bd322771b18a09747766e14f726dad
ysx09621 <yaosx@cookoo.com.cn>
2019-03-21 17:47:59 +0800

    提交hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+#include<stdio.h>
+#define BUFFSIZE 4096
+int main(void)
+{
+
+       
+		
+		printf("ID %ld\n",(long)getpid());
+		return 0;
+
+}                                                  
+
